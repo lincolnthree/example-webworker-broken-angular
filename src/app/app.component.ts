9bb8ec39ec5a9f4de8302182fabe823246d3d8ed
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2021-04-18 16:53:25 -0400

    Observable webworker does not seem compatible with Angular 12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,6 @@
+import { fromWorker } from 'observable-webworker';
+import { Subject } from 'rxjs';
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,19 +10,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'example-web-worker';
-}
 
-if (typeof Worker !== 'undefined') {
-  // Create a new
-  const worker = new Worker('./app.worker', { type: 'module' });
-  worker.onmessage = ({ data }) => {
-    console.log(`page got message: ${data}`);
-  };
-  worker.onerror = ({ error }) => {
-    console.log(`page got ERROR:`, error);
-  };
-  worker.postMessage('hello');
-} else {
-  // Web Workers are not supported in this environment.
-  // You should add a fallback so that your program still executes correctly.
+  constructor() {
+    const input$ = new Subject<string>();
+
+    /*
+     * 1. This syntax results in code that never seems to be called/never resolves.
+     */
+    const getWorkerFailsSilently = () => new Worker(new URL('./app.worker', import.meta.url), { type: 'module' });
+    fromWorker(getWorkerFailsSilently, input$, null, {
+      terminateOnComplete: true
+    })
+      .subscribe({
+        complete: () => console.log('1. getWorkerFailsSilently COMPLETE'),
+        error: error => console.error('1. getWorkerFailsSilently', error),
+        next: msg => console.warn('1. getWorkerFailsSilently', msg)
+      });
+
+
+    /*
+     * 2. This syntax results in code that gets called but results in an error.
+     */
+    const getWorkerFailsWithError = () => {
+      const url = new URL('./app.worker', import.meta.url);
+      const worker = new Worker(url, { type: 'module' });
+      return worker;
+    };
+    fromWorker(getWorkerFailsWithError, input$, null, {
+      terminateOnComplete: true
+    })
+      .subscribe({
+        complete: () => console.log('2. getWorkerFailsWithError COMPLETE'),
+        error: error => console.error('2. getWorkerFailsWithError', error),
+        next: msg => console.warn('2. getWorkerFailsWithError', msg)
+      });
+
+    /*
+     * 3. This syntax results in code that gets called but results in an error (expected since it's not using 'type: "module"').
+     */
+    const getWorkerFailsWithErrorNoModule = () => {
+      const url = new URL('./app.worker', import.meta.url);
+      const worker = new Worker(url);
+      return worker;
+    };
+    fromWorker(getWorkerFailsWithErrorNoModule, input$, null, {
+      terminateOnComplete: true
+    })
+      .subscribe({
+        complete: () => console.log('3. getWorkerFailsWithErrorNoModule COMPLETE'),
+        error: error => console.error('3. getWorkerFailsWithErrorNoModule', error),
+        next: msg => console.warn('3. getWorkerFailsWithErrorNoModule', msg)
+      });
+
+    input$.next('Hello world!');
+  }
 }
--- src/app/app.worker.ts
+++ src/app/app.worker.ts
@@ -1,6 +1,23 @@
 /// <reference lib="webworker" />
 
-addEventListener('message', ({ data }) => {
-  const response = `worker response to ${data}`;
-  postMessage(response);
-});
+import { DoWork, ObservableWorker } from 'observable-webworker';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+@ObservableWorker()
+export class ExampleWorker implements DoWork<string, string> {
+
+  constructor() {
+    console.warn('Created worker');
+  }
+
+  public work(input$: Observable<string>): Observable<string> {
+    return input$.pipe(
+      switchMap(input => {
+        console.warn('Worker received message:', input)
+        return of('message from worker: ' + input);
+      })
+    );
+  }
+
+}
