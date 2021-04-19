146269c4356ba413132785e8b4d986787fb346e6
Zak Henry <zak@cloudnc.com>
2021-04-19 19:39:23 +1200

    fix(Worker) : Fix webworker being minified away by using runWorker() approach

--- src/app/app.worker.ts
+++ src/app/app.worker.ts
@@ -1,10 +1,9 @@
 /// <reference lib="webworker" />
 
-import { DoWork, ObservableWorker } from 'observable-webworker';
+import {DoWork, runWorker} from 'observable-webworker';
 import { Observable, of } from 'rxjs';
 import {switchMap, take} from 'rxjs/operators';
 
-@ObservableWorker()
 export class ExampleWorker implements DoWork<string, string> {
 
   constructor() {
@@ -14,7 +13,7 @@ export class ExampleWorker implements DoWork<string, string> {
   public work(input$: Observable<string>): Observable<string> {
     return input$.pipe(
       switchMap(input => {
-        console.warn('Worker received message:', input)
+        console.warn('Worker received message:', input);
         return of('message from worker: ' + input);
       }),
       take(1)
@@ -22,3 +21,6 @@ export class ExampleWorker implements DoWork<string, string> {
   }
 
 }
+
+
+runWorker(ExampleWorker);
